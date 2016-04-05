a87e33f308b07ffd5d64f4901a25ead8a5072d28
tuca marius <tuca.marius.octavian@gmail.com>
2016-04-06 00:20:49 +0300

    - add new action for Menu

---
+++ Lunch/proj/Lunch.WebApi/Models/DeleteMenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lunch.WebApi.Models
+{
+    public class DeleteMenuModel
+    {
+        public DateTime StartDate{ get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
